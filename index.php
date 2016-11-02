c71d43ec7c052a7fd065334cccc33d3574faf3e3
makfauzan <simg1881@gmail.com>
2016-11-02 17:47:59 +0300

    home page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GPS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div id="bigwrapper">
+      <header id="main_header">
+      <div id="div_header">
+        <p>
+          <h3>ziTrack</h3>
+        </p>
+      </div>
+
+      </header>
+        <nav id="top_menu">
+          <ul>
+            <li><a href="#"></a><p>CARS</p></li>
+            <li><a href="#"></a>PLAYBACK</li>
+            <li><a href="#"></a>STATISTICS</li>
+            <li><a href="#"></a>REPORT</li>
+          </ul>
+        </nav>
+        <div id="new_div">
+      <section id="main_section">
+        <article>
+        <nav id="article_menu">
+          <ul>
+            <li><a href="#">ALL()</a></li>
+            <li><a href="#">ON()</a></li>
+            <li><a href="#">OFF()</a></li>
+          </ul>
+        </nav>
+        </article>
+      </section>
+      <aside id="sideshow">
+        <h2>Google Maps</h2>
+      </aside>
+      </div>
+      <footer id="the_footer">copy right 2016</footer>
+    </div>
+  </body>
+</html>
